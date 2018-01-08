f10c176b8bdbc4988c3e22c1411e87fbde0786aa
General Paul <generalpaul@hotmail.com>
2018-01-08 12:15:48 +0800

    revert settings

--- PPMS_Project/src/settings.js
+++ PPMS_Project/src/settings.js
@@ -13,8 +13,8 @@
 
 export default {
     
-   // serviceName: "http://localhost:30313/odata", //odata url
- //   serviceNameBase: "http://localhost:30313", //base api url
+    serviceName: "http://localhost:30313/odata", //odata url
+    serviceNameBase: "http://localhost:30313", //base api url
 
     // serviceName: "http://absppms01.corp.abscbn.com:8085/odata",
     // serviceNameBase: "http://absppms01.corp.abscbn.com:8085/",
@@ -22,16 +22,16 @@ export default {
     // serviceName: "http://10.0.50.48:8088/odata",
     // serviceNameBase: "http://10.0.50.48:8088:8085/", 
 
-     serviceName: "http://absppms2:8094/odata",
-     serviceNameBase: "http://absppms2:8094",
+    // serviceName: "http://absppms2:8094/odata",
+    // serviceNameBase: "http://absppms2:8094",
 
     // serviceName: "http://absppms01:8085/odata",
     // serviceNameBase: "http://absppms01:8085",
 
   pageSize: 100,
   STATIONS: ["", "CEBU", "DAVAO"],
-  //actualCostWebUrl: "http://localhost:15253",
-  actualCostWebUrl: "http://absppms2:8084", //actual cost url
+  actualCostWebUrl: "http://localhost:15253",
+  //actualCostWebUrl: "http://absppms2:8084", //actual cost url
   actualCostServiceBase: "http://absppms2.corp.abscbn.com:8083", //actual cost service
   
   // actualCostServiceBase: "http://absppms01:8083", //actual cost service
